da0e073bc8bcfdc4f76c399bda7a849020271d6d
MohammadReza <sakhaei.wd@gmail.com>
2021-11-10 17:28:21 +0330

    write password_test

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -10,32 +10,35 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-
 func TestCreateUser(t *testing.T) {
 	createRandomUser(t)
 }
 
-func TestGetUser(t *testing.T){
+func TestGetUser(t *testing.T) {
 	user1 := createRandomUser(t)
 	user2, err := testQueries.GetUser(context.Background(), user1.Username)
 
 	require.NoError(t, err)
 	require.NotEmpty(t, user2)
-	
+
 	require.Equal(t, user1.Username, user2.Username)
-    require.Equal(t, user1.HashedPassword, user2.HashedPassword)
-    require.Equal(t, user1.FullName, user2.FullName)
-    require.Equal(t, user1.Email, user2.Email)
-    require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
-    require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.FullName, user2.FullName)
+	require.Equal(t, user1.Email, user2.Email)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
 }
 
 func createRandomUser(t *testing.T) User {
+
+	hashedPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+	
 	arg := CreateUserParams{
-		Username : util.RandomOwner(),
-		HashedPassword: "secret",
-		FullName: util.RandomOwner(),
-		Email : util.RandomEmail(),		
+		Username:       util.RandomOwner(),
+		HashedPassword: hashedPassword,
+		FullName:       util.RandomOwner(),
+		Email:          util.RandomEmail(),
 	}
 
 	user, err := testQueries.CreateUser(context.Background(), arg)
@@ -49,6 +52,6 @@ func createRandomUser(t *testing.T) User {
 
 	require.NotZero(t, user.CreatedAt)
 	//When the user is first created, we expect this field to be filled with a default value of a zero timestamp
-	require.True(t, user.PasswordChangedAt.IsZero()) 
+	require.True(t, user.PasswordChangedAt.IsZero())
 	return user
 }
---
+++ util/password_test.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+
+func TestPassword(t *testing.T) {
+	password := RandomString(6)
+
+	hashedPassword, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+
+	wrongPassword := RandomString(6)
+    err = CheckPassword(wrongPassword, hashedPassword)
+    require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+}
